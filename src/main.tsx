5786ff8cb455075dbff1beadcc8503a927b5c560
Eray Dindar - Desktop <eray@epctex.com>
2025-11-02 20:20:01 +0300

    feat: scaffold theming

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
+import { ThemeProvider } from '@mui/material'
 import { QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { startWorker } from './mocks/browser'
 import { queryClient } from './services/queryClient.ts'
+import theme from './theme.ts'
 
 async function enableMocking() {
   if (import.meta.env.MODE === 'development') {
@@ -18,7 +20,9 @@ enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </QueryClientProvider>
     </React.StrictMode>,
   )
---
+++ src/theme.ts
@@ -0,0 +1,68 @@
+import { grey } from '@mui/material/colors'
+import { createTheme } from '@mui/material/styles'
+
+const BRAND_RED = {
+  main: '#d23b38',
+}
+
+const TYPOGRAPHY_GRAY = {
+  main: '#2c2c2c',
+  light: '#5d686e',
+}
+
+const theme = createTheme({
+
+  typography: {
+    fontFamily: [
+      'Montserrat',
+      'Roboto',
+      'sans-serif',
+    ].join(','),
+  },
+  palette: {
+    primary: {
+      main: BRAND_RED.main,
+    },
+    text: {
+      primary: TYPOGRAPHY_GRAY.main,
+      secondary: TYPOGRAPHY_GRAY.light,
+      disabled: grey[500],
+    },
+    success: {
+      main: '#1f9b78',
+    },
+    background: {
+      default: '#fafafa',
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 12,
+          fontWeight: 600,
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          color: TYPOGRAPHY_GRAY.main,
+          fontWeight: 500,
+          borderRadius: 12,
+          backgroundColor: '#f4f5f5',
+        },
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          color: TYPOGRAPHY_GRAY.light,
+          fontSize: '1rem',
+        },
+      },
+    },
+  },
+})
+
+export default theme
